18c865abf7c2f2ff172218e8c78b4693d704f3f6
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-03-08 17:13:19 -0300

    Add Ban Method

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import TelegramBot from "node-telegram-bot-api";
 import { BOT_TOKEN } from "./configs/variables.js";
 import { goodbay } from "./functions/goodbay.js";
 import { welcome } from "./functions/welcome.js";
+import { permissionError } from "./tools/ErrorManager.js";
+import { isAdmin } from "./tools/StatusCheker.js";
 
 const token = BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -19,4 +21,41 @@ bot.on("message", function (msg) {
 
   welcome(bot, chatId, chatTitle, msg); // Saluda si entro alguien
   goodbay(bot, chatId, chatTitle, msg); // Se despide si salio alguien
+});
+
+bot.onText(/^\/ban (.+)/, function (msg, match) {
+  var chatId = msg.chat.id;
+  var userId = msg.from.id;
+  var replyId = msg.reply_to_message.from.id;
+  var replyName = msg.reply_to_message.from.first_name;
+  var fromName = msg.from.first_name;
+  var messageId = msg.message_id;
+  var text = match[1];
+  const ms = require("ms");
+
+  if (msg.reply_to_message == undefined) {
+    return;
+  }
+
+  bot.getChatMember(chatId, userId).then(function (data) {
+    if (isAdmin(data.status)) {
+      bot
+        .kickChatMember(chatId, replyId, {
+          until_date: Math.round((Date.now() + ms(text + " days")) / 1000),
+        })
+        .then(function (result) {
+          bot.deleteMessage(chatId, messageId);
+          bot.sendMessage(
+            chatId,
+            "El usuario " +
+              replyName +
+              " ha sido baneado durante " +
+              text +
+              " días."
+          );
+        });
+    } else {
+      permissionError(bot, chatId, fromName);
+    }
+  });
 });
