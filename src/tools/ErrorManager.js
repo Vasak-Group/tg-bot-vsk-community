1ef85242c2993f6cfa18d04d22c9b7630adf604e
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-03-08 17:37:32 -0300

    Add execution error

--- src/tools/ErrorManager.js
+++ src/tools/ErrorManager.js
@@ -4,3 +4,10 @@ export const permissionError = (bot, chatId, fromName) => {
     `Lo siento ${fromName}, no tienes los permisos necesarios para esta acción`
   );
 };
+
+export const executionError = (bot, chatId, fromName) => {
+  bot.sendMessage(
+    chatId,
+    `Lo siento ${fromName} ocurrió un error, intentelo nuevamente`
+  );
+};
